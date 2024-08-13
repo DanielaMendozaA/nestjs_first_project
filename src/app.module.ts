7ece7e3287d463d7d969fb471466911ba1e755b8
DanielaMendozaA <97774155+DanielaMendozaA@users.noreply.github.com>
2024-08-13 10:58:54 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({ //El método forRoot se utiliza para configurar la conexión a la base de datos
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "api_nestjs",
+      // autoLoadEntities: true, //Busca todas las entidades de la base de datos
+      entities: [User],
+      synchronize: true
+    }),
+    UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
